1353a48f7d59278614a901f16e1cbf3019b05eeb
Zhong Zhenyu <nczzy1997@gmail.com>
2020-10-28 20:43:03 +0800

    Refactor plot_kpi.py

--- sample/plot_kpi.py
+++ sample/plot_kpi.py
@@ -1,57 +1,49 @@
 import os
-import utils
-import logging
+import bagel
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 
-from bagel.data import KPI
-from datetime import datetime
-from matplotlib.figure import Figure, Axes
+
+def _expand(a: np.ndarray) -> np.ndarray:
+    ret = np.copy(a)
+    for i in range(length := len(a)):
+        if a[i] == 1:
+            if i - 1 >= 0:
+                ret[i - 1] = 1
+            if i + 1 < length:
+                ret[i + 1] = 1
+    return ret
 
 
-def _plot_abnormal(kpi: KPI, ax: Axes, series: np.ndarray, color: str):
-    split_index = np.where(np.diff(series) != 0)[0] + 1
-    points = np.vstack((kpi.timestamps, kpi.values)).T.reshape(-1, 2)
-    segments = np.split(points, split_index)
-    for i in range(len(segments) - 1):
-        segments[i] = np.concatenate([segments[i], [segments[i + 1][0]]])
-    if series[0] == 1:
-        segments = segments[0::2]
-    else:
-        segments = segments[1::2]
-    for line in segments:
-        ax.plot([datetime.fromtimestamp(timestamp) for timestamp in line[:, 0]], line[:, 1], color)
-
-
-def _plot_kpi(kpi: KPI, fig: Figure):
-    ax: Axes = fig.add_subplot()
-    ax.plot([datetime.fromtimestamp(timestamp) for timestamp in kpi.timestamps], kpi.values)
-    _plot_abnormal(kpi=kpi, ax=ax, series=kpi.labels, color='red')
-    _plot_abnormal(kpi=kpi, ax=ax, series=kpi.missing, color='orange')
-    ax.set_title(kpi.name)
-    ax.set_ylim(-7.5, 7.5)
+def _plot_kpi(kpi: bagel.data.KPI):
+    x = [datetime.datetime.fromtimestamp(timestamp) for timestamp in kpi.timestamps]
+    y_anomaly, y_missing = np.copy(kpi.values), np.copy(kpi.values)
+    y_anomaly[_expand(kpi.labels) == 0] = np.inf
+    y_missing[_expand(kpi.missing) == 0] = np.inf
+    plt.plot(x, kpi.values)
+    plt.plot(x, y_anomaly, color='red')
+    plt.plot(x, y_missing, color='orange')
+    plt.title(kpi.name)
+    plt.ylim(-7.5, 7.5)
 
 
 def main():
-    utils.mkdirs(OUTPUT)
-    file_list = utils.list_file(INPUT)
-    progress = utils.ProgressLogger(len(file_list))
+    bagel.utils.mkdirs(OUTPUT)
+    file_list = bagel.utils.file_list(INPUT)
 
-    fig: Figure = plt.figure(figsize=(16, 4))
-    for file in file_list:
-        kpi = utils.load_kpi(file)
-        progress.log(kpi=kpi.name)
+    plt.figure(figsize=(32, 4))
+    for i in range(total := len(file_list)):
+        kpi = bagel.utils.load_kpi(file_list[i])
+        print(f'Plotting: ({i + 1}/{total}) {kpi.name}')
         kpi, _, _ = kpi.standardize()
-
-        _plot_kpi(kpi=kpi, fig=fig)
-        fig.savefig(os.path.join(OUTPUT, kpi.name + '.png'))
-        fig.clear()
+        kpi.complete_timestamp()
+        _plot_kpi(kpi)
+        plt.savefig(os.path.join(OUTPUT, kpi.name + '.png'))
+        plt.clf()
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='[%(asctime)s [%(levelname)s]] %(message)s')
-
     INPUT = 'data'
-    OUTPUT = 'out/plot'
-
+    OUTPUT = os.path.join('out', 'plot')
     main()
